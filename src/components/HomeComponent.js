c7514c71e5ffd4080afd65e3b813932af1e71f5b
TiagoX9 <tiagojopereira@gmail.com>
2019-07-15 20:12:01 +0200

    Add quote functionality

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -10,29 +10,47 @@ list.push({ index: 3, quote: 'third quote', author: 'third author' });
 export default class HomeComponent extends Component {
     constructor(props) {
         super(props);
+
         this.addQuote = this.addQuote.bind(this);
+
+        this.state = {
+            list: [{ index: 1, quote: 'first quote', author: 'first author' }, { index: 2, quote: 'second quote', author: 'second author' }, { index: 3, quote: 'third quote', author: 'third author' }],
+            selectedQuote: ''
+        }
     }
+
+    componentWillMount() {
+        this.setState({
+            selectedQuote: this.state.list[Math.floor(Math.random() * list.length)],
+        })
+    }
+
+
     addQuote(quote) {
-        list.push({
-            index: list.length + 1,
-            quote: quote.newQuote,
-            author: quote.author
-        });
-        this.setState({ list: quote })
+        this.setState(() => {
+            list.push({
+                index: list.length + 1,
+                quote: quote.newQuote,
+                author: quote.author
+            });
+            return {
+                list,
+                selectedQuote: this.state.list[Math.floor(Math.random() * list.length)]
+            }
+        })
+
+        console.log(this.state.list.length)
     }
     render() {
         return (
             <div>
-                {
-                    list.map((quote, index) => {
-                        return (
-                            <div key={index}>
-                                <QuoteItem quotes={quote.quote} author={quote.author} />
-                            </div>
 
-                        )
-                    })
-                }
+                <div key={this.state.list.index}>
+                    <QuoteItem quotes={this.state.selectedQuote.quote} author={this.state.selectedQuote.author} />
+                </div>
+
+
+
                 <QuoteForm addQuote={this.addQuote} />
             </div>
         )
