06a29c7be75e07e7f9cf4f74723f123142eb6be2
TiagoX9 <tiagojopereira@gmail.com>
2019-08-14 21:13:34 +0200

    added firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBrXEQXj6Ea-txhxE7Rqoc9FkKbcvhBOQQ",
+    authDomain: "random-quote-generator-a39e6.firebaseapp.com",
+    databaseURL: "https://random-quote-generator-a39e6.firebaseio.com",
+    projectId: "random-quote-generator-a39e6",
+    storageBucket: "",
+    messagingSenderId: "779183954873",
+    appId: "1:779183954873:web:0e37b3fe0b34cd93"
+};
